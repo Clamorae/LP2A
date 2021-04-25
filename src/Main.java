2e8bc608def59104a06f0d982b335412497cd199
Clamore <rotuman.lenoob@gmail.com>
2021-04-25 17:53:44 +0200

    comment 1

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,29 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Main implements ActionListener {//this class contain the main method which will call create the Game board and use it to initialize everything else
+public class Main implements ActionListener {//this class contain the main method and all the stuff useful for the initialization
     static Frame f;
 
-    public static void main(String[]args){
-        init();
+    public static void main(String[]args){//this method will create a frame with two button, one for each game, apply the modification and launch the game
+        Main.f = new Frame();
+        f.setSize(400,400);
+        Button player = new Button("Play with friends");
+        player.setSize(400,200);
+        Button cpu = new Button("Play alone");
+        cpu.setSize(400,200);
+        f.add(player);
+        f.add(cpu);
+        player.addActionListener(new Main());
+        cpu.addActionListener(new Main());
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+        f.setVisible(true);
     }
 
-    public static void launchGame(){
+    public static void launchGame(){// in this method the Game board will be initialized and if it's a game with friends it will roll 4 dice to determine who will begin and then launch the game
         Color[] colorArray = {Color.BLUE, Color.RED, Color.GREEN, Color.YELLOW};
         GameBoard gb = new GameBoard(colorArray);
         for(Player p : GameBoard.getPlayers()){
@@ -43,28 +58,8 @@ public class Main implements ActionListener {//this class contain the main metho
 
     }
 
-    public static void init(){
-        Main.f = new Frame();
-        f.setSize(400,400);
-        Button player = new Button("Play with friends");
-        player.setSize(400,200);
-        Button cpu = new Button("Play alone");
-        cpu.setSize(400,200);
-        f.add(player);
-        f.add(cpu);
-        player.addActionListener(new Main());
-        cpu.addActionListener(new Main());
-        f.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent we) {
-                System.exit(0);
-            }
-        });
-        f.setVisible(true);
-    }
-
-
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {//this method is called when the button are clicked, they will set the variable which define the Game mode
         String str = e.getActionCommand();
         if(str.equals("Play with friends")) {
             GameManager.setCpu(false);
